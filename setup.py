5f4b0342169b4b31ce3e4a7f29864404dd380a40
Maesak <maesak28@gmail.com>
2025-06-18 09:02:29 +0530

    main Code&prompt changed

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description="A GenAI application that uses LLMs to classify and process currency conversion requests.",
     version="0.1",
     author="Maesak Delbar",
-    author_email="maesak.delbars@gmail.com",
+    author_email="maesak.delbar@gmail.com",
     packages=find_packages(include=['src', 'src.*']),
     install_requires = requirements,
 )
